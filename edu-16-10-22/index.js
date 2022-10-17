e882654b3ca18d2d99ab82ab9cb6d745b03520db
Alex <mail060920@gmail.com>
2022-10-17 10:58:16 -0700

    17-10-22

---
+++ edu-16-10-22/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.listen(3000, () => console.log('listening on 3000...'))
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+app.post('/api', (request, response) => {
+    console.log('I got request!');
+    console.log(request);
+});
